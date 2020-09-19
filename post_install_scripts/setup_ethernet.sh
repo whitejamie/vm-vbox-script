4da43e22d873c02397a75042bbac9e628b8e023a
Jamie White <jamie@jwhite.dev>
2020-09-19 18:17:56 +0200

    Bugfix for setup_ethernet.sh and improvements to vm-post-install.sh

--- post_install_scripts/setup_ethernet.sh
+++ post_install_scripts/setup_ethernet.sh
@@ -2,5 +2,5 @@
 # Script to enable and connect the ethernet.
 
 ethernet_dev_name=$(nmcli -f TYPE,DEVICE device | grep ^ethernet | awk '{print $2}')
-nmcli con add type ethernet con-name my-network ifname $ethernet_device_name
+nmcli con add type ethernet con-name my-network ifname $ethernet_dev_name
 nmcli con up my-network
--- vm-post-install.sh
+++ vm-post-install.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# Makes an .iso from the post_install_scripts directory and adds it to the VM
+# drive so it's available to be mounted and its scripts executed.
+
 dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 source $dir/lib/get_config.sh
@@ -12,19 +15,20 @@ mkdir -p $temp_dir
 mkisofs -iso-level 3 -o $iso_file $dir/post_install_scripts
 
 
+# First unattach any existing storage
+VBoxManage storageattach $(get_config "vm_name") \
+    --storagectl "IDE Controller" \
+    --port 1 --device 0 --type dvddrive \
+    --medium "emptydrive" \
+    --forceunmount
+
 VBoxManage storageattach $(get_config "vm_name") \
     --storagectl "IDE Controller" \
     --port 1 --device 0 --type dvddrive \
     --medium $iso_file
 
-# Unattach...
-# VBoxManage storageattach $(get_config "vm_name") --storagectl IDE --port 1 --device 0 --medium "none"
-
-
-
-#VBoxManage storageattach $(get_config "vm_name") --storagectl "IDE Controller" --port 1 \
-#    --device 0 --type dvddrive --medium $iso_file
 
 echo "Now log into VM and mount the CDROM device to run the post installation scripts in the .iso..."
 echo "mkdir -p /root/post_install_scripts"
 echo "mount /dev/cdrom /root/post_install_scripts"
+echo "sh /root/post_install_scripts/hello_world.sh"
